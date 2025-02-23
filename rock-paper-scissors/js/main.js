229040bac91db05ac57db0b8532566d6981e0be4
RedCraftGH <jonathonportlock@outlook.com>
2025-02-23 16:32:53 -0500

    Completed Rock, Paper, Scissors Logic In js
    Linked the main.js file to the index.html. Programmed the
    logic for the rock, paper, scissors game.

---
+++ rock-paper-scissors/js/main.js
@@ -0,0 +1,147 @@
+let score = {
+    computer: 0,
+    player: 0
+}
+
+let chosen = {
+    computer: null,
+    player: null
+}
+
+function getRandomInt(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function chooseComputer() {
+
+    let selector = getRandomInt(1, 3);
+    switch (selector) {
+        case 1:
+            chosen.computer = "Rock";
+        break;
+        case 2:
+            chosen.computer = "Paper";
+        break;
+        case 3:
+            chosen.computer = "Scissors";
+        break;
+    }
+    chosen.computer = chosen.computer.toLowerCase();
+
+    console.log("Computer: " + chosen.computer);
+    return chosen.computer;
+}
+
+function choosePlayer(coa) {
+
+    chosen.player = prompt(coa + `\nComputer: ${score.computer}\nPlayer: ${score.player}`);
+    chosen.player = chosen.player.toLowerCase();
+    if (chosen.player === "rock" || chosen.player === "paper" || chosen.player === "scissors") {
+
+        console.log("Player: " + chosen.player);
+        return chosen.player;
+    } else {
+
+        choosePlayer(`You can't choose ${chosen.player}, silly!`);
+    }
+}
+
+function compareChosen() {
+
+    let player = chosen.player;
+    let computer = chosen.computer;
+    if (!player || !computer) {
+
+        alert("Something went terribly wrong.");
+        return;
+    }
+
+    switch (player) {
+
+        case "rock":
+            if (computer === player) {
+
+                alert("It was a tie!");
+            } else if (computer === "paper") {
+
+                increaseComputer();
+                alert(`The computer chose ${computer}! You lose!`);
+            } else {
+
+                increasePlayer();
+                alert(`The computer chose ${computer}! You win!`)
+            }
+        break;
+        case "paper":
+            if (computer === player) {
+
+                alert("It was a tie!");
+            } else if (computer === "scissors") {
+
+                increaseComputer();
+                alert(`The computer chose ${computer}! You lose!`);
+            } else {
+
+                increasePlayer();
+                alert(`The computer chose ${computer}! You win!`)
+            }
+        break;
+        case "scissors":
+            if (computer === player) {
+
+                alert("It was a tie!");
+            } else if (computer === "rock") {
+
+                increaseComputer();
+                alert(`The computer chose ${computer}! You lose!`);
+            } else {
+
+                increasePlayer();
+                alert(`The computer chose ${computer}! You win!`)
+            }
+        break;
+    }
+}
+
+function increasePlayer() {
+
+    return score.player++;
+}
+
+function increaseComputer() {
+
+    return score.computer++;
+}
+
+function playRound() {
+
+    chooseComputer();
+    choosePlayer("Rock, Paper, or Scissors?");
+    compareChosen();
+}
+
+function playGame() {
+
+    while (Math.max(score.player, score.computer) < 5) {
+
+        playRound();
+    }
+
+    declareWinner();
+}
+
+function declareWinner() {
+
+    if (score.player == 5) {
+
+        alert("All games have been played. The winner is the player!" + `\nComputer: ${score.computer}\nPlayer: ${score.player}`);
+    } else {
+
+        alert("All games have been played. The winner is the computer!" + `\nComputer: ${score.computer}\nPlayer: ${score.player}`);
+    }
+}
+
+playGame();
