18d2c8d67c31b3be40f05ac162ff145009feaba6
RedCraftGH <jonathonportlock@outlook.com>
2025-02-27 15:38:46 -0500

    Refactored Old Logic And Added New
    Added more logic for the etch-a-sketch to make it more
    etch-a-sketch like.

--- etch-a-sketch/js/main.js
+++ etch-a-sketch/js/main.js
@@ -1,22 +1,38 @@
-let lengthAndWidth = 100;
+let slider = document.querySelector("#gridsize");
+let screen = document.querySelector(".screen");
+let display = document.querySelector("#display");
+
+const DISPLAY_RATIO = 2;
+const ERROR_MARGIN = 0.999;
+
+let height = slider.value;
+let width = height * DISPLAY_RATIO;
 
 let active = false;
 
-let screen = document.querySelector(".screen");
-
 let pixels = [];
 
-const ERROR_MARGIN = 0.999;
+let pixelIndex = {
+    firstPixel: {
+        x: null,
+        y: null
+    },
+    secondPixel: {
+        x: null,
+        y: null
+    }
+}
 
-function buildEtchSketch(lengthAndWidth) {
+function buildEtchSketch(height) {
 
-    let dimensions = calcDimensions(lengthAndWidth);
+    let dimensions = calcDimensions(height);
+    pixels = [];
     createDivisions(dimensions);
-    initPixels(pixels);
-    console.log(pixels);
 }
 
 function createDivisions(dimensions) {
+
+    let fragment = document.createDocumentFragment();
 
     for (i = 0; i < dimensions[0]; i++) {
 
@@ -24,38 +40,24 @@ function createDivisions(dimensions) {
         pixel.style.width = dimensions[1] + "px";
         pixel.style.height = dimensions[2] + "px";
         pixels.push(pixel);
-        screen.appendChild(pixel);
+        fragment.appendChild(pixel);
     }
+
+    screen.appendChild(fragment);
 }
 
-function calcDimensions(lengthAndWidth) {
+function calcDimensions(height) {
 
-    let area = lengthAndWidth * lengthAndWidth;
-    let pixelWidth = (screen.clientWidth / lengthAndWidth) * ERROR_MARGIN;
-    let pixelHeight = screen.clientHeight / lengthAndWidth;
-
-    console.log([area, pixelWidth, pixelHeight]);
+    let area = height * width;
+    let pixelWidth = (screen.clientWidth / width) * ERROR_MARGIN;
+    let pixelHeight = screen.clientHeight / height;
 
     return [area, pixelWidth, pixelHeight];
 }
 
-function initPixels(pixels) {
-
-    for (let pixel of pixels) {
-
-        pixel.addEventListener('mouseover', updatePixel);
-    }
-}
-
-function updatePixel(e) {
-
-    if (active) {
-
-        e.target.style.backgroundColor = "gray";
-    }
-}
-
 function clearScreen() {
+
+    active = false;
 
     for (let pixel of pixels) {
 
@@ -66,6 +68,7 @@ function clearScreen() {
 function toggleActive() {
     
     active = !active;
+    
 }
 
 function removeDivisions() {
@@ -76,6 +79,151 @@ function removeDivisions() {
     }
 }
 
-window.addEventListener('mousedown', toggleActive);
+function managePixels(e) {
 
-buildEtchSketch(lengthAndWidth);
+    active = true;
+
+    if (e.target !== screen && active) {
+
+        buildPixelIndex(e.target);
+        e.target.style.backgroundColor = "darkgray";
+    }
+}
+
+function rebuildScreen() {
+    
+    height = document.querySelector("#gridsize").value;
+    width = height * DISPLAY_RATIO;
+    active = false;
+    removeDivisions();
+    buildEtchSketch(height);
+}
+
+function updateDisplay() {
+
+    height = document.querySelector("#gridsize").value;
+    width = height * DISPLAY_RATIO;
+    display.innerHTML = `${width}x${height}`;
+}
+
+function findPixels(pixelIndex) {
+
+    let x1 = pixelIndex.firstPixel.x;
+    let y1 = pixelIndex.firstPixel.y;
+
+    let x2 = pixelIndex.secondPixel.x;
+    let y2 = pixelIndex.secondPixel.y;
+
+    let line = [];
+
+    let dx = Math.abs(x2 - x1);
+    let dy = Math.abs(y2 - y1);
+    let sx = x1 < x2 ? 1 : -1;
+    let sy = y1 < y2 ? width : -width;
+    let err = dx - dy;
+    console.log({dx, dy});
+    console.log({sx, sy});
+    console.log({err});
+
+    do {
+        line.push({x: x1, y: y1});
+        console.log({x: x1, y: y1});
+
+        if (dx !== 0 && x1 != x2) {
+
+            x1 += sx;
+            console.log({x1});
+        }
+        if (dy !== 0 && y1 != y2) {
+
+            y1 += sy;
+            console.log({y1});
+        }
+        
+        // if (2 * err > -dy) {
+        //     err -= dy;
+        //     x1 += sx;
+        // }
+        // if (2 * err < dx) {
+        //     err += dx;
+        //     y1 += sy;
+        // }
+    }
+    while (!(x1 == x2 && y1 == y2));
+
+    console.log(line);
+    buildLine(line);   
+}
+
+function buildLine(line) {
+
+    for (const point of line) {
+
+        let pixelId = point.x + point.y;
+        
+        pixels[pixelId].style.backgroundColor = "darkgray";
+    }
+}
+
+function buildPixelIndex(pixel) {
+
+    let pixelId = pixels.indexOf(pixel);
+    let pixelX;
+    let pixelY;
+
+    //compare the pixel id to the pixel id of the first pixel in x row
+    //if greater than
+        //continue to the next row
+        //return to line 1
+    //else
+        //pixel must be on the previous row
+
+    for (i = 0; i <= width * height; i += width) {
+
+        if (pixelId < i) {
+
+            pixelY = i -= width;
+            break;
+        }
+    }
+    pixelX = pixelId - pixelY;
+
+    if (pixelIndex.firstPixel.x === null) {
+
+        pixelIndex.firstPixel.x = pixelX;
+        pixelIndex.firstPixel.y = pixelY;
+        console.log(pixelIndex);
+    } else if (pixelIndex.secondPixel.x === null) {
+
+        pixelIndex.secondPixel.x = pixelX;
+        pixelIndex.secondPixel.y = pixelY;
+
+        findPixels(pixelIndex);
+        console.log(pixelIndex);
+    } else {
+
+        clearPixelIndex();
+        buildPixelIndex(pixel);
+    }
+}
+
+function clearPixelIndex() {
+
+    pixelIndex = {
+        firstPixel: {
+            x: null,
+            y: null
+        },
+        secondPixel: {
+            x: null,
+            y: null
+        }
+    }
+}
+
+// screen.addEventListener('mouseover', managePixels);
+
+screen.addEventListener('mousedown', managePixels);
+
+buildEtchSketch(height);
+updateDisplay();
